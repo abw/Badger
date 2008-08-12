81d1f19276ea787f86a85fdbdf55f93c880ca354
Andy Wardley <abw@wardley.org>
2008-08-12 20:23:39 +0000

    Added accept/reject filters for Badger::Filesystem::Visitor: files, dirs, no_files, no_dirs, in_dirs, not_in_dirs
    git-svn-id: svn://badgerpower.com/Badger/trunk/badger@129 f7e19ab9-6750-0410-bcef-57680045ec6a

--- lib/Badger/Filesystem/Visitor.pm
+++ lib/Badger/Filesystem/Visitor.pm
@@ -16,29 +16,37 @@ use Badger::Class
     version   => 0.01,
     debug     => 0,
     base      => 'Badger::Base',
-    constants => 'ARRAY',
+    constants => 'ARRAY CODE REGEX',
     utils     => 'params',
     messages  => {
-        no_node => 'No node specified to %s'
+        no_node    => 'No node specified to %s',
+        bad_filter => 'Invalid reference to %s %s: %s',
     };
 
 sub init {
     my ($self, $config) = @_;
 
-    for (qw( all recurse )) {
-        $self->{ $_ } = $config->{ $_} || 0;
-    }
-
     # allow 'directories' as alias for 'dirs'
     $self->{ dirs } = $self->{ directories }
         if exists $self->{ directories };
+    $self->{ no_dirs } = $self->{ no_directories }
+        if exists $self->{ no_directories };
+    $self->{ not_in_dirs } = $self->{ not_in_directories }
+        if exists $self->{ not_in_directories };
     
+    for (qw( all recurse no_files no_dirs not_in_dirs )) {
+        $self->{ $_ } = $config->{ $_ } || 0;
+    }
+
     # tread carefully because a value in $config is likely to be false
-    for (qw( files dirs )) {
+    for (qw( files dirs in_dirs )) {
         $self->{ $_ } = defined $config->{ $_} 
             ? $config->{ $_ }
             : 1;
     }
+
+    $self->{ collect  } = [ ];
+    $self->{ identify } = { };
         
     # TODO: accept, ignore, handlers
 
@@ -48,19 +56,7 @@ sub init {
 sub visit {
     my $self = shift;
     my $node = shift || return $self->error_msg( no_node => 'visit' );
-    $self->prepare(@_);
     $node->accept($self);
-    $self->cleanup;
-}
-
-sub prepare {
-    my ($self, $config) = @_;
-    $self->{ config   } = $config;
-    $self->{ collect  } = [ ];
-    $self->{ identify } = { };
-}
-
-sub cleanup {
 }
 
 sub visit_path {
@@ -70,18 +66,21 @@ sub visit_path {
 
 sub visit_file {
     my ($self, $file) = @_;
-    return unless $self->{ files };
     $self->debug("visiting file: $file\n") if $DEBUG;
-    $self->collect($file);
+
+    $self->collect($file) 
+        if $self->accept_file($file);
 }
 
 sub visit_directory {
     my ($self, $dir) = @_;
-    return unless $self->{ dirs };
     $self->debug("visiting directory: $dir\n") if $DEBUG;
-    $self->collect($dir);
+
+    $self->collect($dir) 
+        if $self->accept_directory($dir);
+
     $self->visit_directory_children($dir)
-        if $self->{ recurse };
+        if $self->enter_directory($dir);
 }
 
 sub visit_directory_children {
@@ -89,6 +88,57 @@ sub visit_directory_children {
     $self->debug("visiting directory children: $dir\n") if $DEBUG;
     map { $_->accept($self) }
     $dir->children($self->{ all });
+}
+
+sub filter {
+    my ($self, $type, $name, $method, $item) = @_;
+    my $tests = $self->{ $name } || return 0;
+
+    $self->debug("filter($type, $name, $method, $item)  tests: $tests\n") if $DEBUG;
+    
+    if ($tests eq '1') {
+        return 1;
+    }
+    else {
+        $tests = [$tests]
+            unless ref $tests eq ARRAY;
+    }
+    foreach my $test (@$tests) {
+        $self->debug("  - test: $test\n") if $DEBUG;
+        if (ref $test eq CODE) {
+            return 1 if $test->($item, $self);
+        }
+        elsif (ref $test eq REGEX) {
+            return 1 if $item->$method =~ $test;
+        }
+        elsif (ref $test) {
+            return $self->error_msg( bad_filter => $type => $name => $test );
+        }
+        else {
+            # TODO: handle wildcards, e.g. *.html, foo.* either here or
+            # by pre-constructing regexen.  For now we just match
+            return 1 if $item->$method eq $test;
+        }
+    }
+    return 0;
+}
+
+sub accept_file {
+    my $self = shift;
+    return $self->filter( accept => files    => name => @_ )
+      && ! $self->filter( reject => no_files => name => @_ );
+}
+
+sub accept_directory {
+    my $self = shift;
+    return $self->filter( accept => dirs    => name => @_ )
+      && ! $self->filter( reject => no_dirs => name => @_ );
+}
+
+sub enter_directory {
+    my $self = shift;
+    return $self->filter( accept => in_dirs     => name => @_ )
+      && ! $self->filter( reject => not_in_dirs => name => @_ );
 }
 
 sub collect {
