e8f4ae7d676357810cb99c5fd68afc0686c3ecbd
Andy Wardley <abw@wardley.org>
2008-08-19 09:21:04 +0000

    Added type_args() to Badger::Pod::Nodes to handle Badger::Pod::Node::List (and any others) that require a list of arguments instead of named param hash ref
    git-svn-id: svn://badgerpower.com/Badger/trunk/badger@192 f7e19ab9-6750-0410-bcef-57680045ec6a

--- lib/Badger/Pod/Nodes.pm
+++ lib/Badger/Pod/Nodes.pm
@@ -4,6 +4,7 @@
 #
 # DESCRIPTION
 #   Factory module for loading and instantiating Badger::Pod::Node::*
+#   modules.  Many of the standard node types are also defined below.
 #
 # AUTHOR
 #   Andy Wardley   <abw@wardley.org>
@@ -13,19 +14,44 @@
 package Badger::Pod::Nodes;
 
 use Badger::Class
-    version => 0.01,
-    debug   => 1,
-    base    => 'Badger::Factory';
+    version   => 0.01,
+    debug     => 0,
+    base      => 'Badger::Factory',
+    constants => 'HASH';
 
-our $ITEM      = 'node';
-our $NODE_BASE = ['Badger::Pod::Node', 'BadgerX::Pod::Node'];
-our $NODES     = {
+our $ITEM       = 'node';
+our $NODE_BASE  = ['Badger::Pod::Node', 'BadgerX::Pod::Node'];
+our $NODES      = {
     # any nodes with non-standard mappings from name to module can go here, 
     # but in the general case we grok the module name from the $NODE_BASE
 };
+our $LIST_NODES = {
+    # nodes which are list-based require custom handling of config args
+    map { $_ => 1 }
+    qw( list )              # Badger::Node::List
+};
 
+# map node() and nodes() to base class item() and items() methods
 *node  = __PACKAGE__->can('item');
 *nodes = __PACKAGE__->can('items');
+
+# custom type_args() which handles the case of the 'list' node which is
+# a list-based object expecting a list of node refs rather than a hash
+# config.
+
+sub type_args {
+    my $self   = shift;
+    my $type   = shift;
+    $self->debug("node type: $type ", join(', ', @_), "\n") if $DEBUG;
+    if ($LIST_NODES->{ $type }) {
+        return ($type, @_);
+    }
+    else {
+        my $params = @_ && ref $_[0] eq HASH ? shift : { @_ };
+        $params->{ $self->{ items } } ||= $self;
+        return ($type, $params);
+    }
+}
 
 
 #=======================================================================
