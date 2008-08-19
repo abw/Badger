3f9a17be8ee191dbf6e94327455bd8472a8d0f63
Andy Wardley <abw@wardley.org>
2008-08-19 11:56:26 +0000

    Added Badger::Pod::Node::Class
    git-svn-id: svn://badgerpower.com/Badger/trunk/badger@197 f7e19ab9-6750-0410-bcef-57680045ec6a

---
+++ lib/Badger/Pod/Node/Class.pm
@@ -0,0 +1,128 @@
+#========================================================================
+#
+# Badger::Pod::Node::Class
+#
+# DESCRIPTION
+#   Subclass of Badger::Class which adds some specialised methods/hooks
+#   for creating Badger::Pod::Node::* subclasses.
+#
+# AUTHOR
+#   Andy Wardley   <abw@wardley.org>
+#
+#========================================================================
+
+package Badger::Pod::Node::Class;
+
+use Badger::Class
+    version => 0.01,
+    uber    => 'Badger::Class',
+    hooks   => 'type';
+
+sub self { $_[0] }
+
+sub type {
+    my ($self, $type) = @_;
+
+    $self->methods( 
+        type  => sub { $type },     # $obj->type    ==> $type
+        $type => \&self,            # $obj->$type   ==> $obj
+    );
+}
+
+=head1 NAME
+
+Badger::Pod::Node::Class - class module for Badger::Pod::Node classes
+
+=head1 SYNOPSIS
+
+This module is used by L<Badger::Pod::Nodes> to generate node subclasses.
+
+    package Badger::Pod::Node::Code;
+    
+    use Badger::Pod::Node::Class
+        base => 'Badger::Pod::Node',
+        type => 'code';
+
+    package Badger::Pod::Node::Verbatim;
+    
+    use Badger::Pod::Node::Class
+        base => 'Badger::Pod::Node',
+        type => 'verbatim';
+
+=head1 DESCRIPTION
+
+This module is a subclass of L<Badger::Class> specialised for the purpose
+of creating L<Badger::Pod::Node> subclasses.  It is used by the 
+L<Badger::Pod::Nodes> module as shown in the L<SYNOPSIS> above.
+
+=head1 METHODS
+
+The following methods are provided in addition to those inherited 
+from the L<Badger::Class> base class.
+
+=head2 type($name)
+
+This method defines a L<type()> subroutine for the class which returns
+the value passed as the C<$name> argument.  It is typically called using
+the L<type> import hook.
+
+    package Badger::Pod::Node::Example;
+    
+    use Badger::Pod::Node::Class
+        base => 'Badger::Pod::Node',
+        type => 'example';
+
+Using the C<type> import hook has the same effect as calling the C<type()>
+method on the C<Badger::Pod::Node::Class> object.
+
+    package Badger::Pod::Node::Example;
+    
+    use Badger::Pod::Node::Class
+        base   => 'Badger::Pod::Node',
+        import => 'class';
+    
+    class->type('example');
+
+The effect is to create a C<type()> method in the class's package
+which returns the C<$name> value passed as an argument.
+
+    my $node = Badger::Pod::Node::Example->new;
+    print $node->type;          # example
+
+It also creates a method of the same name as the argument which 
+return the C<$self> object reference.
+
+    $node->example;             # return $node
+
+This is typically used for filtering purposes:
+
+    grep { $_->example } @nodes;
+
+=head1 AUTHOR
+
+Andy Wardley L<http://wardley.org/>
+
+=head1 COPYRIGHT
+
+Copyright (C) 1996-2008 Andy Wardley.  All Rights Reserved.
+
+This module is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+L<Badger::Class>, L<Badger::Pod::Nodes>
+
+=cut
+
+# Local Variables:
+# mode: perl
+# perl-indent-level: 4
+# indent-tabs-mode: nil
+# End:
+#
+# vim: expandtab shiftwidth=4:
+
+
+
+1;
