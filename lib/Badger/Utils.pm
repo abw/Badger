46d380efcb17820512b4d7da0c07b28b2ee0896c
Andy Wardley <abw@wardley.org>
2009-01-10 15:36:45 +0000

    Added numlike() to Badger::Utils
    git-svn-id: svn://badgerpower.com/Badger/trunk/badger@520 f7e19ab9-6750-0410-bcef-57680045ec6a

--- lib/Badger/Utils.pm
+++ lib/Badger/Utils.pm
@@ -51,10 +51,13 @@ our $DELEGATES;         # fill this from $HELPERS on demand
 
 
 __PACKAGE__->export_any(qw(
-    UTILS blessed is_object textlike params self_params plural xprintf dotid
+    UTILS blessed is_object numlike textlike params self_params plural xprintf dotid
 ));
 
 __PACKAGE__->export_fail(\&_export_fail);
+
+# looks_like_number() is such a mouthful.  I prefer numlike() to go with textlike()
+*numlike = \&Scalar::Util::looks_like_number;
 
 sub _export_fail {    
     my ($class, $target, $symbol, $more_symbols) = @_;
@@ -207,6 +210,11 @@ has an overloaded stringification operator.
     my $file = File('example.txt'); 
     print $file;                                # example.txt
     print textlike $file ? 'ok' : 'not ok';     # ok
+
+=head2 numlike($item)
+
+This is an alias to the C<looks_like_number()> function defined in 
+L<Scalar::Util>.  
 
 =head2 params(@args)
 
