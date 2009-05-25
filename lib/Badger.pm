75ecf509c6a9496fafcaacdfacbba9d2a6d485cd
Andy Wardley <abw@wardley.org>
2009-05-25 14:38:58 +0000

    Bumped version number to 0.05_03
    git-svn-id: svn://badgerpower.com/Badger/trunk/badger@658 f7e19ab9-6750-0410-bcef-57680045ec6a

--- lib/Badger.pm
+++ lib/Badger.pm
@@ -6,7 +6,7 @@ use lib;
 use Badger::Hub;
 use Badger::Class
     debug      => 0,
-    version    => '0.05_02',
+    version    => '0.05_03',
     base       => 'Badger::Base',
     import     => 'class',
     words      => 'HUB',
@@ -19,7 +19,7 @@ use Badger::Class
         fail   => \&_export_handler,
     };
 
-our $VERSION = '0.05_02';              # Just for ExtUtils::MakeMaker.  Ick
+our $VERSION = '0.05_03';              # Just for ExtUtils::MakeMaker.  Ick
 our $HUB     = 'Badger::Hub';
 our $AUTOLOAD;
 
