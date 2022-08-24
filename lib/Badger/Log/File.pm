9d4f2022d716b5fdf38edc30b9732daa4b82bf11
Andy Wardley <abw@wardley.org>
2022-08-24 13:06:10 +0100

    Added filename_format to Badger::Log::File

--- lib/Badger/Log/File.pm
+++ lib/Badger/Log/File.pm
@@ -13,19 +13,31 @@
 package Badger::Log::File;
 
 use Badger::Class
-    version    => 0.01,
+    version    => 0.02,
     base       => 'Badger::Log',
     filesystem => 'FS',
+    utils      => 'Now',
     config     => [
         'filesystem|method:FS',
         'filename|class:FILENAME',
+        'filename_format|class:FILENAME_FORMAT',
         'keep_open|class:KEEP_OPEN=0',
     ],
     messages  => {
-        no_filename => 'No filename specified for log file',
+        no_filename => 'No filename or filename_format specified for log file',
         started     => 'Started logging',
     };
 
+our $FORMATS = {
+    DATE    => '%Y-%m-%d',
+    TIME    => '%H-%M-%S',
+    YEAR    => '%Y',
+    MONTH   => '%m',
+    DAY     => '%d',
+    HOURS   => '%H',
+    MINUTES => '%M',
+    SECONDS => '%S',
+};
 
 sub init {
     my ($self, $config) = @_;
@@ -39,17 +51,50 @@ sub init_file {
 
     # init_log() has already called configured() which will copy the filename
     # parameter into $self.  We don't make the filename parameter mandatory
-    # in the config schema because that will report missing parameters 
-    # using error_msg() which has been redefined in Badger::Log.  So we 
+    # in the config schema because that will report missing parameters
+    # using error_msg() which has been redefined in Badger::Log.  So we
     # check for it here and use our "backdoor" _error_msg() method.
-    my $filename = $self->{ filename };
-
     return $self->_error_msg('no_filename')
-        unless defined $filename;
-    
-    $self->{ file } = $self->{ filesystem }->file($filename);
+        unless defined $self->{ filename }
+            || defined $self->{ filename_format };
+
+    # if filename is set but not filename_format then this is a no-op
+    # as expand_filename() will return the filename
+    $self->{ filename } = $self->expand_filename;
 
     $self->info_msg('started');
+}
+
+sub expand_filename {
+    my $self     = shift;
+    my $format   = $self->{ filename_format } || return $self->{ filename };
+    my $strftime = $format;
+    # expand any <XXXX> markers to convert to strftime format
+    $strftime =~ s/<(\w+)>/$FORMATS->{ $1 } || $1/ge;
+    # now format using strftime
+    return Now->format($strftime);
+}
+
+sub filename_changed {
+    my $self     = shift;
+    my $filename = $self->expand_filename;
+    return $filename ne $self->{ filename }
+        ? ($self->{ filename } = $filename)
+        : undef;
+}
+
+sub file {
+    my $self     = shift;
+    my $filename = $self->{ filename };
+    return ($self->{ file } = $self->{ filesystem }->file($filename));
+}
+
+sub filehandle {
+    my $self = shift;
+    if ($self->filename_changed) {
+        $self->release;
+    }
+    return $self->acquire;
 }
 
 sub acquire {
@@ -57,23 +102,19 @@ sub acquire {
 
     # Badger::Filesystem::File::append() method returns file handle open for append
     return $self->{ handle }
-       ||= $self->{ file }->append;
+        ||= $self->file->append;
 }
 
 sub release {
     my $self = shift;
     my $fh   = delete $self->{ handle } || return;
-    $fh->close()
-# TODO: does IO::File throw an error on close fail?
-#        || return $self->base_error_msg( bad_close => $self->{ filename }, $! );
+    $fh->close;
 }
+
 
 sub log {
     my ($self, $level, $message) = @_;
-
-    my $handle = $self->{ handle } 
-        || $self->acquire;
-
+    my $handle = $self->filehandle;
     my $output = sprintf($self->format($level, $message));
     $output =~ s/\n+$//;
     $handle->printflush($output, "\n");
@@ -98,12 +139,12 @@ Badger::Log::File - writes log messages to a log file
 =head1 SYNOPSIS
 
     use Badger::Log::File;
-    
+
     my $log = Badger::Log::File->new({
         filename  => '/var/log/badger.log',
         keep_open => 1,
     });
-        
+
     $log->log('a debug message');
     $log->info('an info message');
     $log->warn('a warning message');
@@ -112,7 +153,7 @@ Badger::Log::File - writes log messages to a log file
 
 =head1 DESCRIPTION
 
-This module is a subclass of L<Badger::Log> that implements a simple 
+This module is a subclass of L<Badger::Log> that implements a simple
 mechanism for logging messages to a file.  It uses L<Badger::Filesystem>
 for all the underlying file operations.
 
@@ -123,8 +164,40 @@ inherited form the L<Badger::Log> base class.
 
 =head2 filename
 
-The name of the log file which you want messages appended to.  This parameter
-is mandatory.
+The name of the log file which you want messages appended to.  Either this or the
+L<filename_format> option must be provided.
+
+=head2 filename_format
+
+A format which can be used to generate a filename to write messages to.
+This can include any of the C<strftime()> character sequences, e.g.
+C<%Y> for the four digit year, C<%m> for a two digit month number, C<%d>
+for a two digit day of the month, etc.
+
+    my $log = Badger::Log::File->new({
+        filename_format  => '/var/log/badger-%Y-%m-%d.log',
+        keep_open => 1,
+    });
+
+If, like me, you find it hard to remember the C<strftime()> character sequences,
+then you can use an alternate format where the elements of the date and/or
+time are encoded as upper case words embedded in angle brackets.
+e.g. C<badger-E<lt>DATEE<gt>.log>, or C<badger-E<lt>YEARE<gt>-E<lt>MONTHE<gt>.log>.
+
+The words that can be embedded this way, and their corresponding C<strftime()> character
+sequences are:
+
+    DATE    => '%Y-%m-%d'       # e.g. 2022-08-24
+    TIME    => '%H-%M-%S'       # e.g. 13:54:07
+    YEAR    => '%Y'             # e.g. 2022
+    MONTH   => '%m'             # e.g. 08
+    DAY     => '%d'             # e.g. 24
+    HOURS   => '%H'             # e.g. 13
+    MINUTES => '%M'             # e.g. 54
+    SECONDS => '%S'             # e.g. 07
+
+The module will automatically create a new logfile when the C<filename_format>
+generates a filename that is different to any previous value.
 
 =head2 filesystem
 
@@ -135,9 +208,9 @@ class.
 
 =head2 keep_open
 
-A flag indicating if the log file should be kept open between calls to 
-logging methods.  The default value is C<0> meaning that the file will be 
-opened for each message and closed again afterwards.  Set it to any true 
+A flag indicating if the log file should be kept open between calls to
+logging methods.  The default value is C<0> meaning that the file will be
+opened for each message and closed again afterwards.  Set it to any true
 value to have the file kept open.
 
 =head1 METHODS
@@ -147,7 +220,7 @@ from L<Badger::Log> and its base classes.
 
 =head2 log($level,$message)
 
-This method redefines the L<log()|Badger::Log/log()> method in 
+This method redefines the L<log()|Badger::Log/log()> method in
 L<Badger::Log> to write logging messages to the log file.
 
 =head2 INTERNAL METHODS
@@ -163,10 +236,41 @@ method.
 Custom initialiser method which handles the configuration and initialisation
 of the file-specific parts of the logger.
 
+=head2 expand_filename()
+
+Expands the format defined by the C<filename_format> configuration option
+to a filename using the current date and time.
+
+If C<filename_format> has not been specified then it returns the value of
+the C<filename> configuration option.
+
+=head2 filename_changed()
+
+Calls L<expand_filename()> to expand any C<filename_format> and compares it
+to the current C<filename> (which may have been generated by a previous call
+to this method).
+
+If they are the same then it returns C<undef>.  Otherwise it saves the new
+filename to the internal C<filename> value and returns it.
+
+=head2 file()
+
+Returns a reference to a L<Badger::Filesystem::File> object based on the
+C<filename> which can either be defined as a static configuration option
+or will be generated by the L<expand_filename()> method from the
+C<filename_format> configuration option.
+
+=head2 filehandle()
+
+Returns a filehandle for the current logfile.  If the filename has changed
+since the last call this method (determined by calling L<filename_changed()>)
+then any existing filehandle is closed (by calling L<release()>) and the new
+file is opened to return a new filehandle (by calling L<acquire()>).
+
 =head2 acquire()
 
 This method acquires a file handle (an L<IO::File> object) for the specified
-L<filename>, opened ready for appending log messages. 
+L<filename>, opened ready for appending log messages.
 
 =head2 release()
 
@@ -179,7 +283,7 @@ Andy Wardley L<http://wardley.org/>
 
 =head1 COPYRIGHT
 
-Copyright (C) 2005-2009 Andy Wardley.  All Rights Reserved.
+Copyright (C) 2005-2022 Andy Wardley.  All Rights Reserved.
 
 This module is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
--- t/log/logfile.t
+++ t/log/logfile.t
@@ -15,12 +15,13 @@ use strict;
 use warnings;
 use lib qw( ./lib ../lib ../../lib );
 use Badger::Test
-    tests => 16,
+    tests => 28,
     debug => 'Badger::Log Badger::Log::File',
     args  => \@ARGV;
 
 use Badger::Filesystem '$Bin Dir';
 use Badger::Log::File;
+use Badger::Timestamp 'Now';
 use constant LOG => 'Badger::Log::File';
 
 my $logdir  = Dir($Bin, 'logs')->must_exist(1);
@@ -33,8 +34,8 @@ $logfile->delete if $logfile->exists;
 # create a log file object
 #------------------------------------------------------------------------
 
-my $log = LOG->new( 
-    filename  => $logfile->path, 
+my $log = LOG->new(
+    filename  => $logfile->path,
     keep_open => 1,
     format    => '[<level>] <message>'
 );
@@ -65,6 +66,45 @@ is( $lines[2], '[warn] this is just a warning', 'first warning' );
 is( $lines[3], '[fatal] this is a fatal error', 'first fatal' );
 
 
+#-----------------------------------------------------------------------------
+# file_format
+#-----------------------------------------------------------------------------
+my $logfile2 = $logdir->file('test-' . Now->format('%Y-%m-%d-%H-%M-%S') . '.log');
+my $logfile3 = $logdir->file('test-' . Now->adjust( seconds => 1 )->format('%Y-%m-%d-%H-%M-%S') . '.log');
+my $logfmt   = $logdir->file('test-<DATE>-<TIME>.log')->path;
+
+$logfile2->delete if $logfile2->exists;
+
+$log = LOG->new(
+    filename_format => $logfmt,
+    keep_open       => 1,
+    format          => '[<level>] <message>'
+);
+
+ok( $log->error('an error'), 'filename_format error' );
+ok( $log->warn('a warning'), 'filename_format warning one' );
+ok( $log->fatal('a fatal error'), 'filename_format fatal one' );
+ok( $logfile2->exists, "logfile2 created: $logfile2" );
+
+@lines = $logfile2->read;
+is( scalar(@lines), 3, 'got 3 lines in logfile2' );
+chomp for @lines;
+is( $lines[0], '[error] an error', 'filename_format error' );
+is( $lines[1], '[warn] a warning', 'filename_format warning' );
+is( $lines[2], '[fatal] a fatal error', 'filename_format fatal' );
+
+sleep 1;
+
+ok( $log->error('next logfile'), 'filename_format next logfile' );
+ok( $logfile3->exists, "logfile3 created: $logfile3" );
+
+@lines = $logfile3->read;
+is( scalar(@lines), 1, 'got 1 line in logfile3' );
+chomp for @lines;
+is( $lines[0], '[error] next logfile', 'filename_format next logfile' );
+
+$logfile2->delete;
+$logfile3->delete;
 
 __END__
 
